dda0a0c807134ab3d6633a6ee653b45d6151f264
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-11-11 21:32:35 +0530

    Player Movement
    Parent Class

---
+++ Movement System/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+     * Master Class for Player Movement
+     */
+    [ContextMenu("Smooth Move/Move X")]
+    public void AddMoveX()
+	{
+        Undo.AddComponent<MoveX>(this.gameObject);
+    }
+    [ContextMenu("Smooth Move/Move Y")]
+    public void AddMoveY()
+    {
+        Undo.AddComponent<MoveY>(this.gameObject);
+    }
+    [ContextMenu("Smooth Move/Move Z")]
+    public void AddMoveZ()
+    {
+        Undo.AddComponent<MoveZ>(this.gameObject);
+    }
+}
