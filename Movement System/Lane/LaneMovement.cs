73538d5487561437d6331797c84ef391f9915c01
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-11-11 21:33:41 +0530

    Lane Movement

---
+++ Movement System/Lane/LaneMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneMovement : MonoBehaviour
+{
+	public KeyCode laneUp, laneDown;
+	[Tooltip("Positive lane distance")]
+	public Vector3 laneDistance;
+	public int numberOfLanes;
+	[Tooltip("lane number starts from 0")]
+	public int currentLaneNumber;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(laneUp) && currentLaneNumber > 0)
+		{
+			currentLaneNumber--;
+			this.transform.position += laneDistance;
+		}
+		if(Input.GetKeyDown(laneDown) && currentLaneNumber < numberOfLanes - 1)
+		{
+			currentLaneNumber++;
+			this.transform.position -= laneDistance;
+		}
+	}
+}
