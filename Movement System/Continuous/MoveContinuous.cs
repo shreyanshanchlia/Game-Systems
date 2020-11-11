e7b40dd064a4d88dd79c28b5b79781d1a6e13417
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-11-11 21:31:42 +0530

    Movement System
     - Continuous movement

---
+++ Movement System/Continuous/MoveContinuous.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveContinuous : MonoBehaviour
+{
+    public Vector3 velocity;
+
+	private void Update()
+	{
+		this.transform.position += velocity * Time.deltaTime;
+	}
+}
