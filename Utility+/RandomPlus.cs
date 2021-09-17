2c2b713a865ba677aff4f85a7ad9abd425f03c0d
Shreyansh Anchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2021-09-17 10:46:26 +0530

    added utility scripts
    1. Always Show Box Collider
    2. RandomPlus for random position within the box collider and to generate random string

---
+++ Utility+/RandomPlus.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public static class RandomPlus
+{
+    /// <summary>
+    /// Get a random position within the box collider.
+    /// </summary>
+    /// <param name="b">this box collider, to get position in collider in.</param>
+    /// <param name="x">should x position be randomized within box collider.</param>
+    /// <param name="y">should y position be randomized within box collider.</param>
+    /// <param name="z">should z position be randomized within box collider.</param>
+    /// <returns>returns random position in box collider.</returns>
+    public static Vector3 RandomPositionInBox(this BoxCollider b, bool x = true, bool y = true, bool z = true)
+    {
+        Vector3 pos;
+        pos.x = x ? Random.Range(- b.size.x / 2, b.size.x / 2) : 0;
+        pos.y = y ? Random.Range(- b.size.y / 2, b.size.y / 2) : 0;
+        pos.z = z ? Random.Range(- b.size.z / 2, b.size.z / 2) : 0;
+
+        pos = Vector3.Scale(pos, b.transform.localScale);
+        pos += b.center + b.transform.position;
+        
+        return pos;
+    }
+
+    /// <summary>
+    /// Create a random string using Unity's Random function.
+    /// </summary>
+    /// <param name="s">this string, that will be used to return a string to, pass a non empty string to concat.</param>
+    /// <param name="length">length of new random string to add on.</param>
+    /// <param name="mixCase">should there be mixed case, otherwise it will return lower case.</param>
+    /// <returns>returns the new randomized string.</returns>
+    public static string RandomString(this string s, int length = 1, bool mixCase = true)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            char c = (char)(mixCase ? Random.Range(0, 1f) > 0.5f ? 'A' : 'a' : 'a' + Random.Range(0, 26));
+            s += c;
+        }
+        return s;
+    }
+}
---
+++ Utility/ShowBowCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Always Show Box Collider. If you still need to hide, then collapse the script, no need of removing
+/// </summary>
+[RequireComponent(typeof(BoxCollider))]
+public class ShowBowCollider : MonoBehaviour
+{
+    private BoxCollider _boxCollider;
+
+    private void OnDrawGizmos()
+    {
+        if (_boxCollider == null)
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+        }
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + _boxCollider.center, Vector3.Scale(_boxCollider.size, transform.localScale));
+    }
+}
